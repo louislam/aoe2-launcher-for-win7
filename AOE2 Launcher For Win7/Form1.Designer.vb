45947323e97112c03abffb4895b19eba11702628
Louis Lam <louislam@live.com>
2015-03-06 14:30:33 +0800

    init git

---
+++ AOE2 Launcher For Win7/Form1.Designer.vb
@@ -0,0 +1,188 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox
+        Me.Hotkey = New System.Windows.Forms.Label
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.autos = New System.Windows.Forms.Timer(Me.components)
+        Me.Button3 = New System.Windows.Forms.Button
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(8, 7)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(148, 52)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Start (1.0c)"
+        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.Visible = False
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(162, 7)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(116, 17)
+        Me.CheckBox1.TabIndex = 1
+        Me.CheckBox1.Text = "Auto start next time"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(162, 30)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(201, 17)
+        Me.CheckBox2.TabIndex = 2
+        Me.CheckBox2.Text = "Enable Color Fix when the game start"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'Hotkey
+        '
+        Me.Hotkey.AutoSize = True
+        Me.Hotkey.Location = New System.Drawing.Point(262, 81)
+        Me.Hotkey.Name = "Hotkey"
+        Me.Hotkey.Size = New System.Drawing.Size(75, 13)
+        Me.Hotkey.TabIndex = 4
+        Me.Hotkey.Text = "Hotkey: Home"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 2000
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(8, 7)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(148, 87)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Start "
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'autos
+        '
+        Me.autos.Enabled = True
+        Me.autos.Interval = 200
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(162, 76)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(94, 23)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Toggle Explorer"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(22, 8)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(96, 13)
+        Me.LinkLabel1.TabIndex = 7
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "Made by LouisLam"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(124, 8)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(63, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Version: 1.0"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.LinkLabel1)
+        Me.GroupBox1.Location = New System.Drawing.Point(-13, 97)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(416, 88)
+        Me.GroupBox1.TabIndex = 9
+        Me.GroupBox1.TabStop = False
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(162, 53)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(211, 17)
+        Me.CheckBox3.TabIndex = 10
+        Me.CheckBox3.Text = "Also close me when the game is closed"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(373, 121)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Hotkey)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.KeyPreview = True
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "LouisLam's AOE2 Launcher for Win7"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+    Friend WithEvents Hotkey As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents autos As System.Windows.Forms.Timer
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents CheckBox3 As System.Windows.Forms.CheckBox
+
+End Class
---
+++ AOE2 Launcher For Win7/Form1.vb
@@ -0,0 +1,176 @@
+﻿Public Class Form1
+
+    Public Declare Function TerminateProcess Lib "kernel32" (ByVal hProcess As IntPtr, ByVal uExitCode As UInteger) As Integer
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        Me.TopMost = True
+
+        If My.Computer.FileSystem.FileExists("age2_x1\age2_x1.exe") = True Then
+            Button1.Visible = True
+            Button2.Text = "Start (1.0)"
+            Button2.Height = Button2.Height - 54
+            Button2.Top = Button2.Top + 54
+        End If
+
+        CheckBox1.Checked = My.Settings.autostart
+        CheckBox2.Checked = My.Settings.fix
+        CheckBox3.Checked = My.Settings.close
+        If CheckBox1.Checked = False Then
+            autos.Enabled = False
+
+        End If
+    End Sub
+
+    Private Sub Form1_FormClosing(ByVal sender As System.Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles MyBase.FormClosing
+        If ProcessesRunning("age2_x1") = 1 Then
+            Dim response As MsgBoxResult
+            response = MsgBox("The game is still running! " & vbNewLine & "Do you really want to close?", MsgBoxStyle.Question + MsgBoxStyle.YesNo, "=_=")
+            If response = MsgBoxResult.Yes Then
+                Me.Dispose()
+            ElseIf response = MsgBoxResult.No Then
+                e.Cancel = True
+                Exit Sub
+            End If
+        End If
+
+    End Sub
+
+    Private Sub Form1_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode = System.Windows.Forms.Keys.Home Then
+            Button3.PerformClick()
+
+            End If
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        My.Settings.Save()
+        System.Diagnostics.Process.Start("age2_x1\age2_x1.exe")
+
+        If CheckBox2.Checked = True Then
+            Timer1.Enabled = True
+            For Each ObjPro As Process In Process.GetProcessesByName("EXPLORER")
+                Do Until TerminateProcess(ObjPro.Handle, 1)
+                    Application.DoEvents()
+                Loop
+            Next
+
+        End If
+
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox1.CheckedChanged
+
+        If CheckBox1.Checked = True Then
+            My.Settings.autostart = True
+
+        Else
+            My.Settings.autostart = False
+
+        End If
+        My.Settings.Save()
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox2.CheckedChanged
+        If CheckBox2.Checked = True Then
+            My.Settings.fix = True
+        Else
+            My.Settings.fix = False
+        End If
+    End Sub
+
+    Public Function ProcessesRunning(ByVal ProcessName As String) As Integer
+        Try
+            Return Process.GetProcessesByName(ProcessName).GetUpperBound(0) + 1
+        Catch
+            Return 0
+        End Try
+    End Function
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        If ProcessesRunning("age2_x1") = 0 Then
+            If ProcessesRunning("explorer") = 0 Then
+                System.Diagnostics.Process.Start("explorer")
+            End If
+
+            Timer1.Stop()
+            If CheckBox3.Checked = True Then
+                End
+            End If
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+
+        System.Diagnostics.Process.Start("age2_x1.exe")
+
+        If CheckBox2.Checked = True Then
+            Timer1.Enabled = True
+            For Each ObjPro As Process In Process.GetProcessesByName("EXPLORER")
+                Do Until TerminateProcess(ObjPro.Handle, 1)
+                    Application.DoEvents()
+                Loop
+            Next
+
+        End If
+    End Sub
+
+    Private Sub autos_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles autos.Tick
+
+        If My.Computer.FileSystem.FileExists("age2_x1\age2_x1.exe") = True Then
+            Button1.PerformClick()
+            autos.Stop()
+        Else
+            Button2.PerformClick()
+            autos.Stop()
+        End If
+        My.Settings.Save()
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        If ProcessesRunning("explorer") = 0 Then
+            System.Diagnostics.Process.Start("explorer")
+        ElseIf ProcessesRunning("age2_x1") = 0 Then
+            Dim response As MsgBoxResult
+            response = MsgBox("The game is not running." & vbNewLine & "Do you really want to use the function?", MsgBoxStyle.Question + MsgBoxStyle.YesNo, "~_~")
+            If response = MsgBoxResult.Yes Then
+                If ProcessesRunning("explorer") = 1 Then
+                    For Each ObjPro As Process In Process.GetProcessesByName("EXPLORER")
+                        Do Until TerminateProcess(ObjPro.Handle, 1)
+                            Application.DoEvents()
+                        Loop
+                    Next
+                End If
+            ElseIf response = MsgBoxResult.No Then
+                Return
+                Exit Sub
+            End If
+        Else
+            If ProcessesRunning("explorer") = 1 Then
+                For Each ObjPro As Process In Process.GetProcessesByName("EXPLORER")
+                    Do Until TerminateProcess(ObjPro.Handle, 1)
+                        Application.DoEvents()
+                    Loop
+                Next
+            End If
+        End If
+
+    End Sub
+
+    Private Sub LinkLabel1_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        System.Diagnostics.Process.Start("http://louislam.coms.hk")
+    End Sub
+
+    Private Sub CheckBox3_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox3.CheckedChanged
+        If CheckBox3.Checked = True Then
+            My.Settings.close = True
+        Else
+            My.Settings.close = False
+        End If
+        My.Settings.Save()
+    End Sub
+End Class
+
